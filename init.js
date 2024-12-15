dbf56c0b590c79587b5587100b9a0eda2a0d0e0f
ROZ a.k.a. Aoi Emerauda <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2024-12-15 15:53:25 +0900

    Update init.js

--- init.js
+++ init.js
@@ -18,6 +18,10 @@ var algos = require('stratum-pool/lib/algoProperties.js');
 
 JSON.minify = JSON.minify || require("node-json-minify");
 
+if (!fs.existsSync('config.json')) {
+    console.log('config.json file does not exist. Read the installation/setup instructions.');
+    process.exit(0);
+}
 
 var portalConfig = JSON.parse(JSON.minify(fs.readFileSync("config.json", { encoding: 'utf8' })));
 var poolConfigs;
